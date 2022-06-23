afe2d6873bb39f870ee882bd3e562addb3b39422
gntsu <tonychan99720@gmail.com>
2022-06-23 20:30:01 +0800

    update

--- Hello.java
+++ Hello.java
@@ -1,9 +1,3 @@
-/*************************************************************************
-	> File Name: Hello.java
-	> Author:xindoo
-	> Mail:ixindoo@gmail.com
-	> Created Time: 四  6/23 20:20:27 2022
- ************************************************************************/
 
 public class Hello{
     public static void main(String[] args){
