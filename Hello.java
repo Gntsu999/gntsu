a79230419a7f56ac0fc0d6cfa635bc1fdcae39c1
gntsu <tonychan99720@gmail.com>
2022-06-23 20:21:58 +0800

    New file

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: Hello.java
+	> Author:xindoo
+	> Mail:ixindoo@gmail.com
+	> Created Time: 四  6/23 20:20:27 2022
+ ************************************************************************/
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello My Friend!");
+    }
+}
+
