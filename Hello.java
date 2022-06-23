534bc00884758fdb6caca0f1cd9cbf30f742368c
gntsu <tonychan99720@gmail.com>
2022-06-23 21:06:17 +0800

    update

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
+/*************************************************************************
+	> File Name: hello.java
+	> Author:gntsu
+	> Mail:tonychan99720@gmail.com
+	> Created Time: 四  6/23 20:54:17 2022
+ ************************************************************************/
 
-public class Hello{
+public class hello{
     public static void main(String[] args){
         System.out.println("Hello My Friend!");
     }
